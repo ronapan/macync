86d6233d18e869fa2e8a6cff0a37c0f62e7aca33
Rona Panlilio <panlilio.rona@marsu.edu.ph>
2026-04-13 18:32:49 +0800

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,10 @@ connectDB();
 const app = express();
 
 // Global Middlewares
-app.use(cors());
+app.use(cors({
+  origin: ["https://macyanc.vercel.app", "http://localhost:5173"], // 
+  credentials: true
+}));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
